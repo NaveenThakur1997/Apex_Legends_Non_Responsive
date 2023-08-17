282fc4c9ff92aed2336b50271925ea0d8c3b77c5
Naveen <naveen.thakur778@gmail.com>
2023-08-18 02:31:16 +0530

    apl

---
+++ script.js
@@ -0,0 +1,11 @@
+var sound1 = new Audio();
+sound1.src = "click.mp3"
+
+var sound2 = new Audio();
+sound2.src="music1.mp3"
+
+var sound3 = new Audio();
+sound3.src="music2.mp3"
+
+var themesound = new Audio();
+themesound.src="apl.mp3"
